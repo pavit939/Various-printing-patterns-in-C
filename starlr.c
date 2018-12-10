95111de0f658b122ad5a7dd37866936398ee3d52
pavit939 <40062888+pavit939@users.noreply.github.com>
2018-12-10 15:04:21 +0530

    Create starlr.c

---
+++ starlr.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int n,i,j;
+    printf("Enter the no of terms:"); 
+    scanf("%d",&n);
+    for(i=1;i<=n;i++) //To iterate by rows
+    {
+        for(j=1;j<=n;j++) //To iterate by columns 
+        {
+            printf("*");
+        }
+        printf("\n"); //New line to start printing next row
+    }
+    
+}
